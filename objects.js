998e9b0d5e9bc08d57baf864eb32071b49037479
Michael McGovern <mmcgovern.wework@gmail.com>
2019-02-23 23:49:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,14 @@
 var playlist = {
-  Takeoff: 'Last Memory'
-}
+  Takeoff: 'Last Memory',
+  Migos: 'Narcos'
+};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artist] = song
   return playlist
-}
+};
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artist]
+  return playlist
+};
